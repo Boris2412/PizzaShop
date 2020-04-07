74c445562d316833f604e5c62408843e160d395b
Boris Bolshakov <boris.bolsh@gmail.com>
2020-04-07 09:02:19 +0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,44 @@
+require 'rubygems'
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  erb 'Can you handle a secret?'
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  # username, phone, datetime
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:colorpicker]
+
+  hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время' }
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+  @title = "Thank you!"
+  @message = "Уважаемый #{@username}, мы ждём вас: #{@datetime} у выбранного парикмахера: #{@barber}, цвет краски: #{@color}."
+
+  # запишем в файл то, что ввёл клиент
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@username}, phone: #{@phone}, date and time: #{@datetime}. Baber: #{@barber}, Color: #{@color} \n"
+  f.close
+
+  erb :message
+end
