8736d72f277d471f047a5af9907564388853e519
Boris Bolshakov <boris.bolsh@gmail.com>
2020-04-08 21:50:15 +0300

    Add rakefile, update gemfile, add Product model

--- Gemfile
+++ Gemfile
@@ -1,3 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', '>= 2.0.8.1'
+gem 'sqlite3'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'sinatra-contrib'
+
+group :development do
+    gem "tux"
+end
--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,10 @@
-$('select[name="colorpicker"]').simplecolorpicker();
+function something()
+{
+    let x = window.localStorage.getItem('aaa');
+
+    x = x * 1 + 1;
+
+    window.localStorage.setItem('aaa', x);
+
+    alert(x);
+};
